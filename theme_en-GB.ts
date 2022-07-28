0d35ef96a1ffcb1874860ef8e4885149f8ecd6da
Bozo the geek <bozolesv2@gmail.com>
2022-07-29 01:56:59 +0200

    feat(translations): first draft for auto generate files using lupdate

--- theme_en-GB.ts
+++ theme_en-GB.ts
@@ -1,4 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_GB">
+<context>
+    <name>GenericOkCancelDialog</name>
+    <message>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <source>Allow video thumbnails</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes,No</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
